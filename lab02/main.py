05bb57e8c6dcb0848bc7c3aa08b4bfb87838db85
Maxim Khabarov <maximkhabarov@yandex.ru>
2024-02-28 23:19:47 +0300

    done third

--- lab02/main.py
+++ lab02/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, abort
+from flask import request, abort, send_file
 import json
 
 app = Flask(__name__)
@@ -16,18 +16,20 @@ def add_new_product():
     if 'name' not in data or 'description' not in data:
         abort(406)
 
-    name, description = data['name'], data['description']
+    name, description, icon = data['name'], data['description'], data['icon']
     idCounter += 1
     products.append({
         'id': idCounter,
         'name': name,
         'description': description,
+        'icon': icon,
     })
 
     return json.dumps({
         "id": idCounter,
         "name": name,
-        "description": description
+        "description": description,
+        "icon": icon,
     })
 
 
@@ -51,18 +53,36 @@ def process_product(product_id):
         return response
     elif request.method == "PUT":
         data = json.loads(request.data)
-        if 'name' in data:
-            product['name'] = data['name']
-        if 'description' in data:
-            product['description'] = data['description']
+        for field in ['name', 'description', 'icon']:
+            if field in data:
+                product[field] = data[field]
         return json.dumps(product)
-
 
 @app.route("/products", methods=["GET"])
 def get_products():
     global products
 
     return json.dumps(products)
+
+@app.route("/product/<int:product_id>/image", methods=["GET", "POST"])
+def image(product_id):
+    global products
+
+    if request.method == "POST":
+        if len(request.files) != 1:
+            abort(406)
+
+        for file in request.files:
+            f = request.files[file]
+            f.save(file)
+            product = find_product_by_id(product_id)
+            product['icon'] = file
+            print(file)
+
+            return "Uploaded"
+    elif request.method == "GET":
+        product = find_product_by_id(product_id)
+        return send_file(product['icon'])
 
 if __name__ == '__main__':
     app.run()
